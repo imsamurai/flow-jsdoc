2a90f2f91d62e9b617b4e3c3885b20f40ac16678
Shamrai Aleksandr <aleksandr.shamrai@together.com>
2021-04-06 13:12:29 +0300

    tests

--- tests/expected_output/19-generics-with-two-types.js
+++ tests/expected_output/19-generics-with-two-types.js
@@ -1,6 +1,23 @@
 /**
  * @property {Map<string, number>} x Some  property
+ * @property {Promise<any>} y Some  property
  */
 class Foo {
-x: Map<string, number>;
+    x: Map<string, number>;
+    y: Promise<any>;
+    /**
+     * @param {Map<string, number>} one
+     * @returns {Map<string, number>}
+     */
+    f(one: Map<string, number>) : Map<string, number> {
+
+    }
+
+    /**
+     * @param {Promise<any>} one
+     * @returns {Promise<any>}
+     */
+    f2(one: Promise<any>) : Promise<any> {
+
+    }
 }
--- tests/input/19-generics-with-two-types.js
+++ tests/input/19-generics-with-two-types.js
@@ -1,5 +1,21 @@
 /**
  * @property {Map<string, number>} x Some  property
+ * @property {Promise<any>} y Some  property
  */
 class Foo {
+    /**
+     * @param {Map<string, number>} one
+     * @returns {Map<string, number>}
+     */
+    f(one) {
+
+    }
+
+    /**
+     * @param {Promise<any>} one
+     * @returns {Promise<any>}
+     */
+    f2(one) {
+
+    }
 }
